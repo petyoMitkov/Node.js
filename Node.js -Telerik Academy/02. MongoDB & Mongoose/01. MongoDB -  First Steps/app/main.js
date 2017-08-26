0d78ac59e43f04b00a69c3882f48200a3a4c3b07
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-08-26 22:43:56 +0300

    Examplem cached Mongo DB  promis

--- Node.js -Telerik Academy/02. MongoDB & Mongoose/01. MongoDB -  First Steps/app/main.js
+++ Node.js -Telerik Academy/02. MongoDB & Mongoose/01. MongoDB -  First Steps/app/main.js
@@ -8,19 +8,35 @@ const databaseName = 'Students';
 
 const connectionString = `mongodb://localhost:27017/chat`;
 
-MongoClient.connect(connectionString)
-    .then((databaseConnection) => {
-        databaseConnection.collection('Names')
-            .insert({
-                firstName: 'Ivan',
-                lastName: 'Petrov',
-                age: '30'
-            })
-            .then((result) => {
-                console.log(result);
-            });
-        console.log('works');
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+const connectionPromis = MongoClient.connect(connectionString);
+
+//Insert data to DB
+/*connection.then((databaseConnection) => {
+    databaseConnection.collection('Names')
+        .insert({
+            firstName: 'Ivan Georgiev',
+            lastName: 'Petrov',
+            age: 30
+        })
+        .then((result) => {
+            console.log(result);
+        });
+    console.log('works');
+})
+.catch((error) => {
+    console.log(error);
+})*/
+
+//Cached DB
+let cacheDb = null;
+
+connectionPromis.then((db) => {
+    return db;
+}).then((db) => {
+    const names = db.collection('Names')
+        .find({})
+        .toArray()
+        .then((result) => {
+            console.log(result);
+        });
+})

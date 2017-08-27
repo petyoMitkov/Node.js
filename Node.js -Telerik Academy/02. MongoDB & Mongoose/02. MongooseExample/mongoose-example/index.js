f7d5db0f0cefe05137e7ae8e68e6189c1959b8fe
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-08-28 00:24:35 +0300

    Example Mogoose Schema and Inctance Method

--- Node.js -Telerik Academy/02. MongoDB & Mongoose/02. MongooseExample/mongoose-example/index.js
+++ Node.js -Telerik Academy/02. MongoDB & Mongoose/02. MongooseExample/mongoose-example/index.js
@@ -11,3 +11,66 @@ db.on('error', (err) => {
 db.on('open', () => {
     console.log("Connect successfully");
 });
+
+// ===================================
+//            Using Schemas
+// ===================================
+/*
+const modelSchema = mongoose.Schema({
+    model: String,
+    releaseDate: Date,
+    priceInDolors: Number,
+    displaySizeInInches: Number
+});
+const modelName = 'Laptop';
+
+const Laptop = mongoose.model(modelName, modelSchema);
+
+const asus = new Laptop({
+    model: 'Asus G752',
+    releaseDate: new Date(2016, 10, 29),
+    priceInDolors: 1799,
+    displaySizeInInches: 17.3
+});
+
+asus.save((err, entry, numAffected) => {
+    console.log(entry);
+    console.log(numAffected);
+});
+*/
+
+// =========================================
+//          Using instance methods
+// =========================================
+const laptopSchema = mongoose.Schema({
+    model: String,
+    releaseDate: Date,
+    priceInDolors: Number,
+    displaySizeInInches: Number
+});
+
+// * Instance methods don't work with arrow function 
+laptopSchema.methods.getDatailedDescription = function() {
+    const productDescription =
+        `Product desctiption:
+        Model: ${this.model}
+        Release date: ${this.releaseDate}
+        Price: ${this.priceInDolors}
+        Display size: ${this.displaySizeInInches}"`;
+
+    return productDescription;
+}
+
+const modelName = 'Laptop2';
+const Laptop2 = mongoose.model(modelName, laptopSchema);
+
+const lenovo = new Laptop2({
+    model: 'Lenogo G580',
+    releaseDate: new Date(2014, 10, 2),
+    priceInDolors: 999,
+    displaySizeInInches: 15
+});
+
+lenovo.save((err, entry) => {
+    console.log(entry.getDatailedDescription());
+});

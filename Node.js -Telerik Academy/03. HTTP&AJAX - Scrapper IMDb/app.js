af207daa725a8ba08d7400317e13cded96bde388
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-09-06 01:26:12 +0300

    Create Class Queue to hold the urls

--- Node.js -Telerik Academy/03. HTTP&AJAX - Scrapper IMDb/app.js
+++ Node.js -Telerik Academy/03. HTTP&AJAX - Scrapper IMDb/app.js
@@ -4,20 +4,43 @@
 const listMoviesUrl = "http://www.imdb.com/search/title?genres=fantasy&title_type=feature&sort=moviemeter,asc&page=1&view=simple&ref_=adv_prv";
 
 const genres = ["fantasy", "horrer", "comedy", "action", "sci-fi"];
+const pagesCount = 2;
 
-//const request = require("request");
 const httpRequester = require("./utils/http-requester");
 const htmlParser = require("./utils/html-parser");
+const dataStructure = require("./data-structures/queue");
 
-httpRequester.get(listMoviesUrl)
-    .then((result) => {
-        const selector = ".col-title span[title] a"; //IMDb in Compact view
-        const html = result.body;
-        return htmlParser.parsSimpleMovie(selector, html);
-    })
-    .then((movies) => {
-        console.log(movies);
-    })
-    .catch((err) => {
-        console.log(err, { colors: true });
-    });
+let urlsQueue = dataStructure.getQueue();
+
+genres.forEach(genre => {
+    for (let i = 0; i < pagesCount; i += 1) {
+        let url = `http://www.imdb.com/search/title?genres=${genre}&title_type=feature&sort=moviemeter,asc&page=${i+1}&view=simple&ref_=adv_prv`;
+        urlsQueue.push(url);
+    }
+});
+
+function getMoviesFromUrl(url) {
+    console.log("Working with: " + url);
+    httpRequester.get(url)
+        .then((result) => {
+            const selector = ".col-title span[title] a"; //IMDb in Compact view
+            const html = result.body;
+            return htmlParser.parsSimpleMovie(selector, html);
+        })
+        .then((movies) => {
+            console.log(movies.length);
+            //start with next page
+            if (urlsQueue.isEmpty()) {
+                return;
+            }
+            setTimeout(() => {
+                getMoviesFromUrl(urlsQueue.pop());
+            }, 2000);
+
+        })
+        .catch((err) => {
+            console.log(err, { colors: true });
+        });
+}
+
+getMoviesFromUrl(urlsQueue.pop());
